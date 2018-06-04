3c971ad0d9c122b664033ea1a3b3638468396bf4
sammarcus <sam.marcus@me.com>
2018-06-04 11:42:33 -0400

    solution

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,13 @@
-# book.rb
-# Add you Book class here
+class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def cobble
+    @condition = "new"
+    puts "Your shoe is as good as new!"
+  end
+end
